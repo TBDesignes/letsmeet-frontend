97ad4bfe7fc6569f46009b8c05df12f4a2a47556
TBDesignes <tbdesignes@gmail.com>
2022-06-18 14:01:55 +0500

    test notification

--- src/page/profile.js
+++ src/page/profile.js
@@ -1,14 +1,35 @@
 import React from 'react';
 import {
+  Button,
+  Container,
+  Paper,
   Stack,
   Typography,
 } from '@mui/material';
 
+const weatherNotify = () => new Notification('Прогноз погоды обновился');
+
+const testNotify = () => {
+  if (!('Notification' in window)) {
+    // eslint-disable-next-line no-alert
+    alert('Уведомления не поддерживаются');
+  } else if (Notification.permission === 'granted') {
+    weatherNotify();
+  } else if (Notification.permission !== 'denied') {
+    Notification.requestPermission((p) => p === 'granted' && testNotify());
+  }
+};
+
 function ProfilePage() {
   return (
-    <Stack direction="column" alignItems="center" justifyContent="center" minHeight="100vh" textAlign="center">
-      <Typography variant="h3">{'<Пустота>'}</Typography>
-    </Stack>
+    <Container>
+      <Paper>
+        <Stack direction="column" alignItems="center" justifyContent="center" textAlign="center" minHeight="300px">
+          <Typography variant="h3">{'<Пустота>'}</Typography>
+        </Stack>
+        <Button onClick={testNotify}>Тест уведомлений</Button>
+      </Paper>
+    </Container>
   );
 }
 
